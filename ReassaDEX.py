9b356c50bb2991b640e3e9e4ec5049a61de8d957
pedross3 <pe.santos91@gmail.com>
2017-03-31 12:18:08 -0300

    working on reassadex

--- ReassaDEX.py
+++ ReassaDEX.py
@@ -1,127 +1,157 @@
-###[
+##[
 ##	{
 ##		"nome" : "picaxu",
 ##		"poder" : 20,
 ##		"vida" : 200,
 ##		"defesa" : 12
+##	},
+##	{
+##		"nome" : "xamando",
+##		"poder" : 25,
+##		"vida" : 100,
+##		"defesa" : 10
+##	},
+##	{
+##		"nome" : "pidijet",
+##		"poder" : 15,
+##		"vida" : 300,
+##		"defesa" : 10
 ##	}
+##]
 [
         {
                 "nome" :        "Dilmasauro",
                 "HP" :	        45,
                 "Attack" :	49,
                 "Defense" :	49,
-		"Specs" :       "Doa o próprio item pra quem vai substituí-lo",
-        }
+		"Specs" :       "Doa o próprio item pra quem vai substituí-lo"
+        },
         {
                 "nome" :        "Leninsaur",
                 "HP" : 	        60,
                 "Attack" :	62,
                 "Defense" :	63,
-		"Specs" :       "Doa o próprio item pra quem vai substituí-lo", 
-        }
+		"Specs" :       "Doa o próprio item pra quem vai substituí-lo"
+        },
         {
                 "nome":         "Marx-o-saur",
                 "HP" : 	        80,
                 "Attack" :      82,
                 "Defense" :	83,
-		"Specs" :       "Doa o próprio item pra quem vai substituí-lo",
+		"Specs" :       "Doa o próprio item pra quem vai substituí-lo"
         
-        }
+        },
         {
                 "nome" :        "Chavandoo",
                 "HP" :	        39,
                 "Attack":	52,
                 "Defense" :	43,
-                "Specs":        "Quando invocado, print Hello, world!",
-        }
+                "Specs":        "Quando invocado, print Hello, world!"
+        },
         {
                 "nome" :        "Charmhilton",
                 "HP" : 	        58,
                 "Attack" :	64,
                 "Defense" :	58,
-                "Specs" : "Quando invocado, print Hello, world!"
-        }
+                "Specs" :       "Quando invocado, print Hello, world!"
+        },
         {
                 "nome" :        "Ayreszard",
                 "HP" : 	        78,
                 "Attack" :	84,
                 "Defense" :     78,
 		"Specs":        "Quando invocado, print Hello, world!"
-        }
-
+        },
         {
                 "nome" :        "Escroto",
                 "HP" : 	        "44",
                 "Attack" :	48,
                 "Defense" :	65,
-                "Specs" :       "Te deixa incomodado",
-        }
+                "Specs" :       "Te deixa incomodado"
+        },
         {
-                "nome" :        "Wartorto"
+                "nome" :        "Wartorto",
                 "HP" :  	59,
                 "Attack" : 	63,
                 "Defense" : 	80,
-        }
+                "Specs" :       "Te deixa incomodado"
+        },
         {
                 "nome" :        "Blastranho",
                 "HP" : 	        79,	
                 "Attack" :	83,
                 "Defense" :	100,
-		"Specs" :       "Te deixa incomodado",
-        }
+		"Specs" :       "Te deixa incomodado"
+        },
         {
                 "nome" :        "Impeachmonlee",
                 "HP" : 	        50,
                 "Attack":	120,
                 "Defense":	53,
-        }
+                "Specs" :       "Quando entra em campo, subtitui o Inspermon adversário"
+        },
         {
                 "nome" :        "Impeachmonchan"
                 "HP" :  	50,
                 "Attack" :	105,
                 "Defense" : 	79,
-        }
+                "Specs" :       "Quando entra em campo, subtitui o Inspermon adversário"
+        },
         {
-                "nome" : "Impeachmontop"
-	HP	50
-	Attack	95
-	Defense	95
-		Specs: Quando entra em campo, subtitui o Inspermon adversário
-
-Rattiti (Com os atributos base de Ratata)= Ratibike
-	HP	30
-	Attack	56
-	Defense	35
-Ratata (Atributos criados) = Ratinete
-	HP	43
-	Attack	66
-	Defense	47
-Raticate = Ratiskate
-	HP	55
-	Attack	81
-	Defense	60
-		Specs: 35% de chance a mais de começar a batalha pelo seu turno, e de fugir dela.
-
-Skarmory = Escárnio
-	HP	65
-	Attack	80
-	Defense	140
-		Specs: Abaixa defesa após desferir um poema de escárnio
-
-Caterpie = Carapinha
-	HP	45
-	Attack	30
-	Defense	35
-Metapod = Meterpode
-	HP	50
-	Attack	20
-	Defense	55
-Butterfree = Manteiga Livre
+                "nome" :        "Impeachmontop"
+                "HP" :  	50,
+                "Attack" :	95,
+                "Defense" :	95,
+		"Specs" :       "Quando entra em campo, subtitui o Inspermon adversário"
+        },
+        {
+                "nome" :        "Rattibike",
+                "HP" :  	30,
+                "Attack" :	56,
+                "Defense" :	35,
+                "Specs": "35% de chance a mais de começar a batalha pelo seu turno, e de fugir dela."
+        },
+        {
+                "nome" :        "Ratinete",
+                "HP" : 	        43,
+                "Attack" :	66,
+                "Defense" :	47,
+                "Specs": "35% de chance a mais de começar a batalha pelo seu turno, e de fugir dela."
+        },
+        {
+                "nome" :        "Ratiskate",
+                "HP" : 	        55,
+                "Attack":	81,
+                "Defense" : 	60,
+		"Specs": "35% de chance a mais de começar a batalha pelo seu turno, e de fugir dela."
+        },
+        {
+                "nome" :        "Escárnio",
+                "HP" : 	        65,
+                "Attack" :	80,
+                "Defense" : 	140,
+		"Specs" :       "Abaixa defesa após desferir um poema de escárnio"
+        },
+        {
+                "nome" :        "Carapinha",
+                "HP" : 	        45,
+                "Attack" : 	30,
+                "Defense" : 	35,
+                "Specs" :       ""
+        },
+        {
+                "nome" :        "Meterpode",
+                "HP" :	        50,
+                "Attack" :	20,
+                "Defense" :	55,
+                "Specs" : ""
+        },
+        {
+                "nome" : Butterfree = Manteiga Livre
 	HP	60
 	Attack	45
 	Defense	50
-		Specs: 
+		"Specs" : 
 
 Dratini = Bolsomini
 	HP	41
