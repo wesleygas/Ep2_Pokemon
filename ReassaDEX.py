052e77811e13937ad5fffb77c12d3ced89d73ec2
pedross3 <pe.santos91@gmail.com>
2017-03-31 11:26:32 -0300

    Added ReassaDEX.py in json structure

---
+++ ReassaDEX.py
@@ -0,0 +1,318 @@
+###[
+##	{
+##		"nome" : "picaxu",
+##		"poder" : 20,
+##		"vida" : 200,
+##		"defesa" : 12
+##	}
+[
+        {
+            "nome" : "Dilmasauro",
+            "HP" :	45,
+            "Attack" :	49,
+            "Defense" :	49,
+        }
+        {
+            "nome" : "Leninsaur",
+            "HP" : 	60,
+            "Attack" :	62,
+            "Defense" :	63,
+Venusaur = Marx-o-saur
+	HP	80
+	Attack	82
+	Defense	83
+		Specs: Doa o próprio item pra quem vai substituí-lo
+
+Charmander = Chavandoo
+	HP	39
+	Attack	52
+	Defense	43
+Charmeleon = Charmhilton
+	HP	58	
+	Attack	64
+	Defense	58
+Charizard = Ayreszard
+	HP	78
+	Attack	84
+	Defense	78
+		Specs: Quando invocado, print("Hello, world!")
+
+Squirtle = Escroto
+	HP	44
+	Attack	48
+	Defense	65
+Wartortle = Wartorto
+	HP	59
+	Attack	63
+	Defense	80
+Blastoise = Blastranho
+	HP	79	
+	Attack	83
+	Defense	100
+		Specs: Te deixa incomodado.
+
+Hitmonlee = Impeachmonlee
+	HP	50
+	Attack	120
+	Defense	53
+Hitmonchan = Impeachmonchan
+	HP	50
+	Attack	105
+	Defense	79
+Hitmontop = Impeachmontop
+	HP	50
+	Attack	95
+	Defense	95
+		Specs: Quando entra em campo, subtitui o Inspermon adversário
+
+Rattiti (Com os atributos base de Ratata)= Ratibike
+	HP	30
+	Attack	56
+	Defense	35
+Ratata (Atributos criados) = Ratinete
+	HP	43
+	Attack	66
+	Defense	47
+Raticate = Ratiskate
+	HP	55
+	Attack	81
+	Defense	60
+		Specs: 35% de chance a mais de começar a batalha pelo seu turno, e de fugir dela.
+
+Skarmory = Escárnio
+	HP	65
+	Attack	80
+	Defense	140
+		Specs: Abaixa defesa após desferir um poema de escárnio
+
+Caterpie = Carapinha
+	HP	45
+	Attack	30
+	Defense	35
+Metapod = Meterpode
+	HP	50
+	Attack	20
+	Defense	55
+Butterfree = Manteiga Livre
+	HP	60
+	Attack	45
+	Defense	50
+		Specs: 
+
+Dratini = Bolsomini
+	HP	41
+	Attack	64
+	Defense	
+Dragonair = Bolsonair
+	HP
+	Attack
+	Defense
+Dragonite = Bolsomight
+	HP
+	Attack
+	Defense
+		Specs: Seu ataque nunca é diminuído
+	
+
+Nidoran F = Merkelran
+	HP
+	Attack
+	Defense
+Nidorina = Merkelrina
+	HP
+	Attack
+	Defense
+Nidoqueen = Merkelqueen 
+	HP
+	Attack
+	Defense
+		Specs: Durante o turno inimigo, o Reassamon ganha um leque de opções, enquanto print("Estou sentindo uma treta!")
+	
+
+Psyduck = Skafduck, Paulo Squack 
+	HP
+	Attack
+	Defense
+Golduck = O Pato, MBLduck
+	HP
+	Attack
+	Defense
+		Specs:  Nunca gasta poção, porque ele não vai pagar o pato
+
+Seel = Selo
+	HP
+	Attack
+	Defense
+Dewgong = Inmetro
+	HP
+	Attack
+	Defense
+		Specs: 
+
+Electrike = Teslatrike
+	HP
+	Attack
+	Defense
+Manectric = Thomactric
+	HP
+	Attack
+	Defense
+		Specs: 
+
+Plusle = Rutherford
+	HP
+	Attack
+	Defense
+Minun = Thomson
+	HP
+	Attack
+	Defense
+	"A ideia da descoberta das cargas elétricas"
+		Specs:
+
+Drowzee = Drowziu
+	HP
+	Attack
+	Defense
+Hypno = Hypnela
+	HP
+	Attack
+	Defense
+		Specs: 
+
+Lapras = Lepras
+	HP
+	Attack
+	Defense
+		Specs: Envenena o adversário ao contato
+
+Magikarp = Magikratos, Manokarp
+	HP
+	Attack
+	Defense
+Gyarados = Manoirados
+	HP
+	Attack
+	Defense
+		Specs: "Nasci pronto." Enquanto magikarp, splash. Gyarados, ele ganha +x em um atributo randômico por ter virado Gyarados.
+
+Mew = Neo (Matrix)
+	HP
+	Attack
+	Defense
+Mewtwo = NeoDois
+	HP
+	Attack
+	Defense
+		Specs: "Acordado". NeoDois não voltará para a Matrix.
+
+Carvania = CarVânia
+	HP
+	Attack
+	Defense
+Sharpedo = Sharkinado
+	HP
+	Attack
+	Defense
+		Specs: 
+
+Totodile = Todos-os-Dados
+	HP
+	Attack
+	Defense
+Croconaw = Criminal
+	HP
+	Attack
+	Defense
+Feraligatr = Feralicate
+	HP
+	Attack
+	Defense
+		Specs:
+
+Girafarig = Girafariz
+	HP
+	Attack
+	Defense
+		Specs:
+
+Wurmple = Rampou
+	HP
+	Attack
+	Defense
+Silcoon = Siliconha
+	HP
+	Attack
+	Defense
+Dustox = Detox
+	HP
+	Attack
+	Defense
+		Specs: "Re-hab": Tem chance de se recuperar de um envenenamento a cada turno.
+
+Lunatone = Lulatone
+	HP
+	Attack
+	Defense
+Sunrock = SenhorRock
+	HP
+	Attack
+	Defense
+		Specs: "Coração de pedra": não morrem com um só golpe.
+
+Tropius = Trumpius
+	HP
+	Attack
+	Defense
+		Specs: Põe uma Magic Wall.
+		Specs: Nega as mudanças de status no inimigo.
+
+Cranidos = Crânios
+	HP
+	Attack
+	Defense
+Rampardos = Retardados
+	HP
+	Attack
+	Defense
+		Specs: Tem 20% de dano aumentado, mas x% de errá-lo, independentemente do ataque.
+
+Cherubi = Chorume
+	HP
+	Attack
+	Defense
+		Specs: Recebe dano reduzido após determinada procentagem de HP.
+
+Staravia = Lavavia
+	HP
+	Attack
+	Defense
+Staraptor = Lavajaptor
+	HP
+	Attack
+	Defense
+		Specs: 
+
+Finneon = Morfinneon
+	HP
+	Attack
+	Defense
+Lumineon = Crackineon
+	HP
+	Attack
+	Defense
+		Specs: "Pro-erd": Tem chance de dizer não à danos.
+
+Probopass = Drop-the-Bass
+	HP
+	Attack
+	Defense
+		Specs: "Drop the bass". O Reassamon 
+
+Snorlax = Skrorllax
+	HP
+	Attack
+	Defense
+		Specs:
+
+]
