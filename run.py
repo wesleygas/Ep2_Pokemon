7033bfe54c809994c60a129c6f65843b89c19665
wesleygas <wesleygl16@gmail.com>
2017-04-20 21:24:13 -0300

    Atualização no código de fuga

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import random
 def run():
     if (bool(random.getrandbits(1))):
-        print ("Run, Forrest. Run!")
+        print ("Você se salvou dessa vez, mas lembre-se:\nQuem nasceu pra Magikarp nunca vai ser Gyarados")
+        return True
+    else:
+    	print("Volte para a batalha, soldado!")
