bf45e15525d75fa55c0ea5d6e400124d7685677e
wesleygas <wesleygl16@gmail.com>
2017-04-21 17:40:39 -0300

    Correção de erros e atualização do código de batalha
    Otimização do código de batalha para trabalhar com o json

--- battle_function.py
+++ battle_function.py
@@ -1,10 +1,36 @@
 #A função receberá os dois reassamons a lutarem e assumirá o controle dos turnos até que o usuário fuja
+#Cada pokémon tem um número na lista de reassamons, teremos que trabalhar com esse número para conseguir
+#as specs do pokemon. Ou seja, "wild" é o número do pokémon na lista "ReassaDEX". No caso do "player",
+#A lista de specs será puxada do arquivo de save, separada da ReassaDEX(que é só um balde de pokemons)
+#Na qual o  index 0 é o nome do jogador(Se ele quiser colocar), e o 1 é o pokémon dele
 def batalha(player, wild):
-	while(player["vida"] > 0 and wild["vida"] > 0):
- 		 wild["vida"] =  wild["vida"] – ( PoderJogador –  wild["defesa"] )
- 		if (wild["vida"] <= 0):
+	VidaJogador = save[1]["vida"]
+	PoderJogador = save[1]["poder"]
+	DefesaJogador = save[1]["defesa"]
+	VidaWild = Reassadex[wild]["vida"]
+	PoderWild = Reassadex[wild]["poder"]
+	DefesaWild = Reassadex[wild]["defesa"]
+	fugir = False
+	opFugir = "x"
+
+	while(VidaJogador > 0 and VidaWild > 0 and fugir = False):
+ 		 VidaWild =  VidaWild – ( PoderJogador –  DefesaWild)
+ 		if (VidaWild <= 0):
  			return "Parabéns, você saudou a mandioca!"
- 		VidaJogador = VidaJogador – (  wild["poder"] – DefesaJogador )
- 		if (player["vida"] <= 0):
+
+ 		VidaJogador = VidaJogador – (  PoderWild – DefesaJogador )
+
+ 		if (VidaJogador <= 0):
 
  			return "YOU HAVE FAILED THIS COUNTRY"
+ 		
+ 		Print("Resumo da rodada:\nSua vida: {0}\nVida do oponente:{1}\n".format(VidaJogador,VidaWild))
+ 		
+ 		while(opFugir != "s" and != "n"):
+ 			opFugir = input("Quer tentar fugir? (s/n)")
+ 			if(opFugir == "s"):
+ 				fugir = run()
+ 			elif(opFugir == "n"):
+ 				break
+ 			else:
+ 				print("Opção inválida, tente novamente")
