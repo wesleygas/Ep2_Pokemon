8520f232472a43ad0b45aba9aa06908fd89cf427
wesleygas <wesleygl16@gmail.com>
2017-04-23 22:11:50 -0300

    Uns Arquivos trimmados e outros renomeados
    a parte um agora é realmente o jogo inteiro e os outros arquivos foram
    excluídos

--- run.py
+++
@@ -1,8 +0,0 @@
-import random
-def run():
-    if (bool(random.getrandbits(1))):
-        print ("Você se salvou dessa vez, mas lembre-se:\nQuem nasceu pra Magikarp nunca vai ser Gyarados")
-        return 1
-    else:
-    	print("Volte para a batalha, soldado!")
-    	return 0
